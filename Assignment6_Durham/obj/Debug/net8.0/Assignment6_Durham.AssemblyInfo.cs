ef75b15a2357f89bab674b80d43849a636a382ad
sarah <sdurham1@byu.edu>
2025-02-19 19:08:23 -0700

    One more commit

--- Assignment6_Durham/obj/Debug/net8.0/Assignment6_Durham.AssemblyInfo.cs
+++ Assignment6_Durham/obj/Debug/net8.0/Assignment6_Durham.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Assignment6_Durham")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de99d238986721aeb490a37fbf9192c664a72d69")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4be48006f2b5066cf7c4e07e37e00603f38b94b8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Assignment6_Durham")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Assignment6_Durham")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
